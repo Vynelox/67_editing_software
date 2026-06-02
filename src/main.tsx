93af39ad674fb6477d0ecbe0e9e86fdc8f6dcf1d
exetude-create <exetude@gmail.com>
2026-06-02 15:32:49 +0800

    made the app stop opening colorpicker every time the app starts (old feature used for debugging, no longer need it)

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { OpenColorPicker } from './components/ColorPicker';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
-
-// Programmatically open the color picker on app start
-try {
-  // small timeout to ensure DOM is ready
-  setTimeout(() => {
-    OpenColorPicker();
-  }, 50);
-} catch (e) {}
